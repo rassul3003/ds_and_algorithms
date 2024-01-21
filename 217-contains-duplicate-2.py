2c203a2fa136264135ed77ceb4a4881b03d6b1f3
rassul3003 <122845680+rassul3003@users.noreply.github.com>
2024-01-20 22:46:52 -0500

    improving code

--- 217-contains-duplicate-2.py
+++ 217-contains-duplicate-2.py
@@ -2,42 +2,53 @@
 # return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
 # Example 1:
+# ----------
 # Input: nums = [1,2,3,1], k = 3
 # Output: true
 
 # Example 2:
+# ----------
 # Input: nums = [1,0,1,1], k = 1
 # Output: true
 
 # Example 3:
+# ---------
 # Input: nums = [1,2,3,1,2,3], k = 2
 # Output: false
 
-arr = [1,2,3,5,3]
-            
+
+# Constraints:
+# -----------
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# 0 <= k <= 105
+
+
+nums = [1,2,3,1]
+k = 3            
 
 # Time Complexity O(n^2)
-def containsDuplicates(arr):
-    num = len(arr)
-    for i in range(num-1):
-        for j in range(i+1, num):
-            if arr[i] == arr[j]:
+def containsNearbyDuplicate_bruteforce(nums, k):
+    n = len(nums)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if nums[i]==nums[j] and abs(i-j) <= k:
                 return True
-    
     return False
+
 
 # Time Complexity O(n)
-def containsDuplicate_hashmap(nums):
-    seen = {}
-    for num in nums:
-        if num in seen:
-            seen[num] += 1
-            if seen[num] >= 2:
-                return True
+def containsNearbyDuplicate_dict(nums, k):
+    d = {}
+    # iterate over indices and corresponding values of list 'nums' and store in dict 'd', where key is number, and value is its index
+    for idx, val in enumerate(nums):
+        if val in d and abs(idx - d[val]) <= k:
+            return True
         else:
-            seen[num] = 1
+            d[val] = idx
+
     return False
 
 
-print('Using bruteforce -', containsDuplicate_hashmap(arr))
-print('Using hashmap -', containsDuplicates(arr))
+print('Using bruteforce -', containsNearbyDuplicate_bruteforce(nums, k))
+print('Using dictionary -', containsNearbyDuplicate_dict(nums, k))
--- 242-valid-anagram.py
+++ 242-valid-anagram.py
@@ -2,19 +2,28 @@
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
 # typically using all the original letters exactly once.
 
+
 # Example 1:
+# ----------
 # Input: s = "anagram", t = "nagaram"
 # Output: true
 
 # Example 2:
+# ----------
 # Input: s = "rat", t = "car"
 # Output: false
+
+
+# Constraints:
+# ------------
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
 
 s = "anagram"
 t = "nagaram"
 
 # Time Complexity O(n)
-def isAnagram_bruteforce(s, t):
+def isAnagram_dictionary(s, t):
     chars_1 = {}
     for char in s:
         if char in chars_1:
@@ -29,10 +38,7 @@ def isAnagram_bruteforce(s, t):
         else:
             chars_2[char] = 1 
 
-    if chars_1 == chars_2:
-        return True
-    else:
-        return False
+    return chars_1 == chars_2
 
 
 # Time Complexity O(nlogn)
@@ -43,5 +49,49 @@ def isAnagram_sorting(s,t):
     return sorted_s == sorted_t
 
 
-print('Using bruteforce -',isAnagram_bruteforce(s,t))
-print('Using sorting -',isAnagram_sorting(s,t))
+
+# needs to import 'defaultdict' from 'collections' library.
+from collections import defaultdict
+
+# CoderPad allows to import the following libraries for python: 'requests', 'beautifulsoup', 'numpy', 'scipy', 'pandas', 'scikit-learn', and  'statsmodels'.
+
+# Time Complexity O(n)
+def isAnagram_defaultdict(s,t):
+    # the difference between defaultdict and regular dict is that if key doesn't exist in reg dict, 'KeyError' will be raised. In defaultdict it will output the default value
+    count = defaultdict(int)
+
+    for x in s:
+        count[x] += 1
+
+    for x in t:
+        count[x] -= 1
+
+    for val in count.values():
+        if val != 0:
+            return False
+        
+    return True
+
+
+# Time Complexity O(n)
+def isAnagram_array(s,t):
+    # initialize the list with 26 elements (for English alphabet) all set to 0, to keep track of frequencies.
+    count = [0]*26
+    
+    for char in s:
+        # find the Unicode for every character using 'ord' f-n, subtract from letter 'a' to store in array at proper index
+        count[ord(char)-ord('a')] += 1
+
+    for char in t:
+        count[ord(char)-ord('a')] -= 1
+
+    for val in count:
+        if val != 0:
+            return False
+    
+    return True
+
+print('Using bruteforce -', isAnagram_dictionary(s,t))
+print('Using sorting -', isAnagram_sorting(s,t))
+print('Using default dictionary -', isAnagram_defaultdict(s,t))
+print('Using array -', isAnagram_array(s,t))
